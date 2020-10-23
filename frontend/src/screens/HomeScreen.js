0a75f5917018185d2a752872dfa5d7a50a10bd9c
NovaTron007 <dwayne.man@gmail.com>
2020-10-23 17:24:20 +0800

    Created the HomeScreen component. Created the Product card component. HomeScreen component mapping product to Product component.

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import products from "../products";
+import Product from "../components/Product"; // map products to this child component
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest products</h1>
+      <Row>
+        {/* map products */}
+        {products.map(product => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} /> {/* pass to product component */}
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
